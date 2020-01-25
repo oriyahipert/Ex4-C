a9102075d6051528b61acb35f81da40005030a59
oriyahipert <57597109+oriyahipert@users.noreply.github.com>
2020-01-25 22:56:07 +0200

    final

--- Trie.c
+++ Trie.c
@@ -12,10 +12,10 @@ char* check_letter(char* words) // convert upper case to lower case
 
     while (str[i])
     {
-		 if(str[i] < 65 || str[i] > 123) //delet if 
-            { 
-				memmove(str[i], &str[i + 1], strlen(str) - i);
-            }
+        if(str[i] < 65 || str[i] > 123) //delet if
+        {
+            memmove(&str[i], &str[i + 1], strlen(str) - i);
+        }
 
         if (str[i] >= 65 && str[i] <= 90)
             str[i] += 32;
@@ -27,7 +27,6 @@ char* check_letter(char* words) // convert upper case to lower case
 
 node* create_node(char *letter) // make new node
 {
- //   printf("create_node\n");
     node* new_node = (node*)malloc(sizeof(node));
     int i = 0;
     new_node->letter = *letter;
@@ -57,75 +56,69 @@ node* create_head()
 
 void insert(node *head, char* str) //insert new node to trie
 {
-            printf("in\n");
 
-	// start from root node
-//    node* curr = *head;
-	char* copy =check_letter(str);
-	while (*copy)
-	{
-		// create a new node if path doesn't exists
-		if (head->children[*copy - 'a'] == NULL)
-			head->children[*copy - 'a'] = create_node(str);
+    if (!*str)
+        return;
+    //char* copy =check_letter(str);
+    while (*str)
+    {
+        // create a new node if path doesn't exists
+        if (head->children[*str - 'a'] == NULL)
+            head->children[*str - 'a'] = create_node(str);
 
-		// go to next node
-		head = head->children[*copy - 'a'];
+        // go to next node
+        head = head->children[*str - 'a'];
 
-		// move to next character
-		copy++;
-	}
-
-	head->end_word = true;
+        // move to next character
+        str++;
+    }
+    head->end_word = true;
     head->count++;
 }
 
-void print_words(node* root, char word[], int temp) //print trie
-{
-            printf("print\n");
 
- //node* head = *root;
-   if(root == NULL)
-      return;
-   
-   if(root->end_word == true)
-   {
-        word[temp]='\0';
-        printf("%s \t %d \n",word,root->count);
-   }
-   for(int i=0; i<NUM_LETTERS; i++)
-   {
-      if(root->children[i] != NULL)
-      {
-         word[temp] = i+'a';
-         print_words((root->children[i]), word, temp+1);
-      }
-   }
+void print_words(node* root, char word[], int temp){
+       
+    if (root -> end_word){
+        word[temp] ='\0';
+        printf("%s", word);
+        printf(" %lu\n", root -> count);
+    }
+    int i;
+    for(i = 0; i < NUM_LETTERS; i++){
+        if (root -> children[i]){
+            word[temp] = i + 'a';
+            print_words((root -> children[i]),word,temp + 1);
+        }
+    }
 }
 
-void print_words_R(node* root, char word[], int temp) //print trie in revers 
+
+
+void print_words_R(node* root, char word[], int temp) //print trie in revers
 {
-          printf("printR\n");
- //node* head = *root;
-   if(root == NULL)
-      return;
-   
-   if(root->end_word == true)
-   {
+
+    //node* head = *root;
+    if(root == NULL)
+        return;
+
+    if(root->end_word == true)
+    {
         word[temp]='\0';
-        printf("%s \t %d \n",word,root->count);
-   }
-   for(int i=NUM_LETTERS-1 ; i>= 0; i--)
-   {
-      if(root->children[i] != NULL)
-      {
-         word[temp] = i+'a';
-         print_words((root->children[i]), word, temp+1);
-      }
-   }
+        printf("%s \t %ld \n",word,root->count);
+    }
+    for(int i=NUM_LETTERS-1 ; i>= 0; i--)
+    {
+        if(root->children[i] != NULL)
+        {
+            word[temp] = i+'a';
+            print_words_R((root->children[i]), word, temp+1);
+        }
+    }
 }
 
 void freeTree (node* head){
-     for (int i = 0; i < NUM_LETTERS; i++ ){
+    for (int i = 0; i < NUM_LETTERS; i++ ){
         if(head -> children[i] != NULL){
             freeTree(head -> children[i]);
         }
@@ -134,4 +127,3 @@ void freeTree (node* head){
         free(head);
     }
 }
-
--- main.c
+++ main.c
@@ -1,132 +1,63 @@
-
 #include "Trie.h"
 #include <string.h>
 int main(int argc, char *argv[]){
     node* root = create_head();
+    int start = 0;
     int index = 0;
-    int max = 0;
     char* word = (char*)malloc(sizeof(char)*2); 
     if (word == NULL){
-        perror("malloc error");
+        printf("Error");
     }
     *word = 0;
     *(word+ 1) = 0;  
     char letter;
-        while (scanf("%c",&letter) == 1 ){
-        if(letter != '\0' && letter != '\t' && letter != '\n' && letter != ',' && letter != ' '){
-            word[index] = letter;
-            index++;   
-            word = (char*)realloc(word,index + 2);  
+        while ((letter = getc(stdin) )!= EOF){
+        if(letter != '\0' && letter != '\t' && letter != '\n' && letter != ',' &&letter != ' '){
+            if(!((letter >= 'A' && letter<= 'Z') || (letter>='a' && letter<='z'))){
+
+                continue;
+            }
+            word[start] = letter;
+            start++;   
+            word = (char*)realloc(word,start + 2);  
               if (word == NULL){
-                  perror("malloc error");
+                printf("Error");
                  }
-            for (int i = index; i < index + 2; i++){
+            for (int i = start; i < start + 2; i++){
                 *(word + i) = 0;
             }       
-        }else{
+        }
+        else{
             insert(root,word);
-            printf("in\n");
             free(word);
-            printf("f/n");
             word = (char*)malloc(sizeof(char)*2);
               if (word == NULL){
-                  perror("malloc error");
+                  printf("Error");
                  }
                 *word = 0;
              *(word+ 1) = 0; 
-            if (index >= max){
-                max = index;
+            if (start >= index){
+                index = start;
             }
-            index = 0;
+            start = 0;
         } 
       }
-        if (index > 0){
+        if (start > 0){
             insert(root,word);
             free(word);
         }
-        if (index == 0){
+        if (start == 0){
             free(word);
         }
-        char length[max];
+        char* length = (char*) malloc(sizeof(char) * index);
     if (argc == 1){
         print_words(root,length,0);
-        printf("pppp/n");
     }else if(argc == 2){
          if (strcmp(argv[1],"r")==0){
          print_words_R(root,length,0);
-         printf("rrrrr/n");
          }
     }
+    free(length);
          freeTree(root);
     return 0;
 }
-
-
-// #include <stdio.h>
-// #include <string.h>
-// #include <stdlib.h>
-// #include "Trie.h"
-// #define MAX 35
-
-// int main(int argc, char* argv[]){
-
-//    int print_R=0;
-//    int size = 256;
-//    int count = 0;
-//    int test = 0;
-//    char* word = (char*)malloc(sizeof(char)*256);
-//    char* temp = NULL;
-//    char c = getchar();
-//    node* head = create_head();
-   
-//      if(argc == 2 && strcmp(argv[1],"r")==0)
-//      print_R = 1;
-
-//      while(c!=EOF || (c==EOF && test!= 1)){
-//          if(c>=65 && c<=90){
-//              c=c+32;
-//              }
-//          //  check_letter(c);
-//          if( c!=' ' && c!= '\n' && c!='\0' && c!='\t'){
-//              if(count==size){
-//                  temp=(char*)realloc(word,(size+MAX)*sizeof(char));
-//                  size+=MAX;
-//                  if(temp==NULL&& word!=NULL){
-//                      free(word);
-//                      return -1;
-//              } 
-//                 word=(char*)calloc(size,sizeof(char));
-//                 strcpy(word,temp);
-//                 free(temp);
-//              }
-        
-//              word[count]=c;
-//              count++;
-            
-             
-//          }
-//          if((c==' ' || c=='\n' || c=='\t' || c=='\0') && c!=EOF){
-//              word[count]='\0';
-//              if(word[0]!=' ' && count>=1){
-//              insert(&head,word);
-//              }
-//              count=0;
-//          }
-//         if(c==EOF)
-//          test=1;
-//        c=getchar();
-//      }
-
-    
-//    if(print_R == 1) {
-//        print_words_R(&head,word,0);
-//    }
-//    else {
-//        print_words(&head,word,0);
-//    }
-//     freeTree(&head);
-//     free(word);
-   
-
-// return 0;
-// }
